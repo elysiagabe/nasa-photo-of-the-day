1afe721f7770da88c987d3c9f513ede4014b0b1d
Elysia Gabe <elysiagabe@gmail.com>
2020-02-19 13:10:13 -0800

    Creates header in App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from "react";
 import "./App.css";
+import Logo from "./Assets/nasa-logo.png"
 
 function App() {
   return (
     <div className="App">
+      <div className="App-header">
+        <img class="App-logo" src={Logo} alt="NASA logo" />
+        <h1>NASA's Astronomy Photo of the Day</h1>
+      </div>
+      
       <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun 🚀!
