097e842d12410d82ef5c57593774ae87f30e76cf
Elysia Gabe <elysiagabe@gmail.com>
2020-02-20 16:10:37 -0800

    Adds Tailwind download button component

--- src/App.js
+++ src/App.js
@@ -7,10 +7,10 @@ import Footer from "./Components/Footer/Footer";
 function App() {
   return (
     <div>
-      <div class="bg-indigo-900 text-center py-4 lg:px-4">
+      <div class="bg-gray-800 text-center py-4 lg:px-4">
       <a href="https://officialnasagear.com/?gclid=Cj0KCQiA-bjyBRCcARIsAFboWg0HRc6_JfplEZtymPho3JY7L_KWo8wlHpRZCvE5HkLNXO4SOFP7QVQaAkr7EALw_wcB">
-          <div class="p-2 bg-indigo-800 items-center text-indigo-100 leading-none lg:rounded-full flex lg:inline-flex" role="alert">
-            <span class="flex rounded-full bg-indigo-500 uppercase px-2 py-1 text-base font-bold mr-3">Love NASA?</span>
+          <div class="p-2 bg-blue-800 items-center text-indigo-100 leading-none lg:rounded-full flex lg:inline-flex" role="alert">
+            <span class="flex rounded-full bg-red-600 uppercase px-2 py-1 text-base font-bold mr-3">Love NASA?</span>
             <span class="font-semibold mr-2 text-left flex-auto text-lg">Support their astronauts &amp; get the coolest gear from their store</span>
             <svg class="fill-current opacity-75 h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M12.95 10.707l.707-.707L8 4.343 6.586 5.757 10.828 10l-4.242 4.243L8 15.657l4.95-4.95z"/></svg>
           </div>
--- src/Components/ImageContainer/ImageContainer.js
+++ src/Components/ImageContainer/ImageContainer.js
@@ -11,6 +11,9 @@ const MainSection = styled.div`
     background-color: #152133;
     color: #fff;
     padding: 60px 10%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
 `;
 
 const ImageContainer = () => {
@@ -43,6 +46,10 @@ const ImageContainer = () => {
     return (
         <MainSection>
             <Image imgSrc={imageData.url}/>
+            <button className="bg-gray-300 hover:bg-gray-400 text-4xl text-gray-800 font-bold py-2 px-4 m-4 rounded inline-flex items-center">
+                <svg className="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z"/></svg>
+                <span>Download</span>
+            </button>
             <ImageInfo 
                 title={imageData.title} 
                 date={imageData.date} 
